7a880247a2ac522d4e86b46f7111e368b94b005c
Ido Talmor <ifw.talmor@gmail.com>
2025-01-21 12:46:02 +0200

    more work, input working

--- cs_intro/hw_3/q2/q2.c
+++ cs_intro/hw_3/q2/q2.c
@@ -9,13 +9,13 @@
 #define HIDDEN ' '
 
 // Function Declarations
-void print_welcome_message(void);
-void print_wrong_board_number(void);
-void print_enter_position(void);
-void print_error_row_or_col(void);
-void print_error_position_already_bombed(void);
+void print_welcome_message();
+void print_wrong_board_number();
+void print_enter_position();
+void print_error_row_or_col();
+void print_error_position_already_bombed();
 void print_winning_message(int n_submarines, int n_moves);
-void printMatrix(char *matrix);
+void printMatrix(char matrix[ROWS][COLS]);
 
 
 // Optional Boards
@@ -41,11 +41,11 @@ const char MATRIX_5[ROWS][COLS] = {{'S', '~', 'S', '~', '~', '~', '~', 'S'}, {'~
                                    {'~', '~', '~', 'S', '~', '~', '~', '~'}, {'~', 'S', '~', '~', '~', '~', 'S', '~'},
                                    {'~', '~', '~', '~', '~', '~', '~', '~'}, {'S', '~', '~', 'S', '~', '~', '~', 'S'}};
 
-void print_welcome_message(void) { printf("Welcome to Battleship! Please enter board number:\n"); }
-void print_wrong_board_number(void) { printf("Error in board number, try again\n"); }
-void print_enter_position(void) { printf("Please enter position:\n"); }
-void print_error_row_or_col(void) { printf("Error in row or column - out of bound\n"); }
-void print_error_position_already_bombed(void) { printf("This position was already bombed - try again!\n"); }
+void print_welcome_message() { printf("Welcome to Battleship! Please enter board number:\n"); }
+void print_wrong_board_number() { printf("Error in board number, try again\n"); }
+void print_enter_position() { printf("Please enter position:\n"); }
+void print_error_row_or_col() { printf("Error in row or column - out of bound\n"); }
+void print_error_position_already_bombed() { printf("This position was already bombed - try again!\n"); }
 void print_winning_message(int n_submarines, int n_moves) {
     printf("Congratulations, Admiral!\nYou've successfully "
            "revealed all %d submarines in %d moves!\n",
@@ -53,7 +53,7 @@ void print_winning_message(int n_submarines, int n_moves) {
 }
 
 // Print a ROWSxCOLS matrix
-void printMatrix(char *matrix) {
+void printMatrix(char matrix[ROWS][COLS]) {
     // Print column headers
     printf("  ");
     for (int j = 0; j < COLS; j++) {
@@ -78,15 +78,22 @@ void get_valid_loc(int *row_ret, int *col_ret) {
     char col = ' ';
     int row = -1;
 
-    while (!((scanf("%d", &row) != 1 && row >= 0 && row <= 7) &&
-             (scanf("%c", &col) != 1 && col >= 'A' && col <= 'H'))) {
+    while (true) {
+        print_enter_position();
+        bool valid = true;
+        if (scanf("%d %c", &row, &col) != 2)
+            valid = false;
+        if (!(row >= 0 && row <= 7 && col >= 'A' && col <= 'H'))
+            valid = false;
+        if (valid)
+            break;
         print_error_row_or_col();
     }
     *row_ret = row;
     *col_ret = (int) (col - 'A');
 }
 
-void cp_board(const char *from, char *to) {
+void cp_board(const char from[8][8], char to[8][8]) {
     for (int r = 0; r < ROWS; r++) {
         for (int c = 0; c < COLS; c++) {
             to[r][c] = from[r][c];
@@ -94,37 +101,46 @@ void cp_board(const char *from, char *to) {
     }
 }
 
-// Add your functions here
-char *get_board_from_user() {
-    int board_num;
-    while (!(scanf("%d", &board_num) != 1 && board_num >= 1 && board_num <= 5)) {
-        print_wrong_board_number();
-    }
+void get_board(char board[8][8], int board_num) {
+
     if (board_num == 1)
-        return (char *) (*MATRIX_1);
+        cp_board(MATRIX_1, board);
     if (board_num == 2)
-        return (char *) (*MATRIX_2);
+        cp_board(MATRIX_2, board);
     if (board_num == 3)
-        return (char *) (*MATRIX_3);
+        cp_board(MATRIX_3, board);
     if (board_num == 4)
-        return (char *) (*MATRIX_4);
+        cp_board(MATRIX_4, board);
     if (board_num == 5)
-        return (char *) (*MATRIX_5);
-    else
-        return NULL;
+        cp_board(MATRIX_5, board);
+}
+
+// Add your functions here
+void get_board_from_user(char board_copy[8][8]) {
+    int board_num = -1;
+    while (true) {
+        if (scanf("%d", &board_num) != 1) {
+            print_wrong_board_number();
+            continue;
+        }
+        if (!(board_num >= 1 && board_num <= 5))
+            print_wrong_board_number();
+        else
+            break;
+    }
+    get_board(board_copy, board_num);
 }
 
 int main(void) {
     print_welcome_message();
-    char *board = get_board_from_user(), board_2[8][8] = {0}, *mut_board[] = *board_2;
-    cp_board(board, mut_board);
-    printMatrix((&mut_board));
-
+    char mut_board[ROWS][COLS] = {0};
+    get_board_from_user(mut_board);
+    printMatrix(mut_board);
 
     int row = -1, col = -1;
     while (true) {
         get_valid_loc(&row, &col);
-        break;
+        printf("pos: %d, %d", row, col);
     }
 
 
